71a6511691833ddb5d9820895717ea1e6648a42a
abhinand v k <abhinandazokdio@gmail.com>
2023-01-14 23:38:10 +0530

    updated

--- src/screens/home_screen/HomeScreen.tsx
+++ src/screens/home_screen/HomeScreen.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const HomeScreen = () => {
   return (
-    <View>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>HomeScreen</Text>
     </View>
   );
--- src/screens/plus_model/PlusModel.tsx
+++ src/screens/plus_model/PlusModel.tsx
@@ -2,11 +2,7 @@ import {View, Text} from 'react-native';
 import React from 'react';
 
 const PlusModel = () => {
-  return (
-    <View>
-      <Text>PlusModel</Text>
-    </View>
-  );
+  return null;
 };
 
 export default PlusModel;
